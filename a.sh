a0fac4a049c2bd767264f1cee23474cb9d465b8b
root <oniga.constantin1@gmail.com>
2019-03-10 19:44:06 +0200

    updates

--- a.sh
+++ a.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sudo yum install git
+hostname=$(awk -F ":" '{if("hostname"==$1)print $2}' /root/conf)
+echo $hostname
 
-git clone https://github.com/CostelOniga/tema1_mlmos.git /root/newdir
-
-echo "s-a clonat"
-
+networks=($(awk -F ":" '{if("network"==$1)print $2}' /root/conf))
+networks=$(echo $networks | tr "," "\n")
+for net in $networks; do
+   ifup $net > echo $?
+done
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 logfile=/var/log/system-bootstrap.log
-config_file="$(dirname $0)/conf/file.conf
+config_file=root/conf
 
+#redirect ouput
 exec 2>&1 >> $logfile
 
-echo "am reusit"
+
+hostname=$(awk -F:'{if("hostname"==$1)print $2}' $config_file)
+echo Se seteaza Hostname: "$hostname"
+hostnameclt set-hostname "$hostname"
+
+echo Se configureaza retele
+networks=($(awk -F ":" '{if("network"==$1)print $2}' $config_file))
+networks=$(echo $networks | tr "," "\n")
+for net in $networks; do
+   Se configureaza reteaua: "$net"
+   ifup $net
+done
+
+
