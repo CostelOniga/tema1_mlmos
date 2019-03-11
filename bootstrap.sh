770355c4ac78555a9460d0809957859af9fdac1d
root <oniga.constantin1@gmail.com>
2019-03-11 10:54:15 +0200

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,10 @@ config_file=root/conf
 exec 2>&1 >> $logfile
 
 
-hostname=($(awk -F:'{if("hostname"==$1)print $2}' $config_file))
+hostname=($(awk -F ":" '{if("hostname"==$1)print $2}' $config_file))
 echo Se seteaza Hostname: "$hostname"
 sudo hostnameclt set-hostname $hostname
-
+echo $?
 echo Se configureaza retele
 networks=($(awk -F ":" '{if("network"==$1)print $2}' $config_file))
 networks=$(echo $networks | tr "," "\n")
@@ -18,6 +18,7 @@ for net in $networks
 do
    echo Se configureaza reteaua: "$net"
    sudo ifup $net
+   echo $?
 done
 
 update=($(awk -F ":" '{if("updates"==$1 && "true"==$2) print $2 }' $config_file))
@@ -25,4 +26,13 @@ if [[ $update == true ]]
 then
    echo se actualizeaza programele
 fi
+
+programs=($(awk -F ":" {if("install"==$1)print $2}' $config_file))
+programs=$(echo $programs | tr "," "\n")
+for prog in $programs
+do
+   echo Se instaleaza : "$prog"
+   sudo yum install $prog
+   echo $?
+done
 
