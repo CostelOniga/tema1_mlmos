d0a961d4acc7e7bc5023d897b1aad3ab272f9f14
root <oniga.constantin1@gmail.com>
2019-03-11 10:31:33 +0200

    updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,18 +9,20 @@ exec 2>&1 >> $logfile
 
 hostname=$(awk -F:'{if("hostname"==$1)print $2}' $config_file)
 echo Se seteaza Hostname: "$hostname"
-sudo hostnameclt set-hostname "$hostname"
+sudo hostnameclt set-hostname $hostname
 
 echo Se configureaza retele
 networks=($(awk -F ":" '{if("network"==$1)print $2}' $config_file))
 networks=$(echo $networks | tr "," "\n")
-for net in $networks; do
-   Se configureaza reteaua: "$net"
+for net in $networks
+do
+   echo Se configureaza reteaua: "$net"
    sudo ifup $net
 done
 
 update=($(awk -F ":" '{if("update"==$1 && "true"==$2) print $2 }' $config_file))
-if [[ $update == true ]] then
+if [[ $update == true ]] 
+then
    echo se actualizeaza programele
 fi
 
